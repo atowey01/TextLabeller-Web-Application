b124c1bddbb643784fd0c9c60fed98da9ba96bbe
Aisling Towey <aislingtowey@gmail.com>
2021-04-10 21:33:49 +0100

    update config file

--- text_labeller/config.py
+++ text_labeller/config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
-    # MAIL_USE_TLS = True
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
